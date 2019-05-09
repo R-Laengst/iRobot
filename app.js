62e90dc69299afc637888641ee3358dc0f70d314
Fabian Bees <fabianbees@web.de>
2019-05-09 11:15:01 +0200

    Refactoring, renaming of example, set Title of webpage

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 
 tabBar.listen('MDCTabBar:activated', (activatedEvent) => {
-  document.querySelectorAll('.adopt-a-pup-body').forEach((element, index) => {
+  document.querySelectorAll('.irobot-body').forEach((element, index) => {
     if (index === activatedEvent.detail.index) {
-      element.classList.remove('adopt-a-pup-body--hidden');
+      element.classList.remove('irobot-body--hidden');
     } else {
-      element.classList.add('adopt-a-pup-body--hidden');
+      element.classList.add('irobot-body--hidden');
     }
   });
 });
